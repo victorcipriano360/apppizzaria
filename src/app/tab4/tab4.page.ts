32e7f3b03046b65765d4529f9b333535485fd019
victorcipriano360 <125392545+victorcipriano360@users.noreply.github.com>
2023-11-15 15:26:49 -0300

    Tabs 4 - com falhas0.1
    Falhas ao tentar inplementar na tab 4
    gostaria de fazer o perfil de usuario mas não consegui, o programa apresentou erros NVM e mesmo instalando n consegui, irei buscar a solução logo mais

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+// Importe os módulos necessários do Angular e Ionic
+import { Component } from '@angular/core';
+import { UserService } from 'caminho/do/seu/servico/user.service'; // Importe o serviço do usuário, substitua pelo caminho correto
 
 @Component({
-  selector: 'app-tab4',
-  templateUrl: './tab4.page.html',
-  styleUrls: ['./tab4.page.scss'],
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
 })
-export class Tab4Page implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ProfilePage {
+  userName: string = ''; // Variável para armazenar o nome do usuário
+  // Injete o serviço do usuário no construtor
+  constructor(private userService: UserService) {
+    this.carregarDadosPerfil(); // Carregue os dados do perfil ao entrar na página
   }
 
+  carregarDadosPerfil() {
+    // Simulando uma requisição para carregar dados do perfil ao entrar na página
+    this.userService.getProfileData().subscribe((data: any) => {
+      // Ao receber os dados, atribua os valores aos campos correspondentes, como userName
+      this.userName = data.name;
+      // Carregue outros dados do perfil, se houverem
+    });
+  }
+
+  salvarPerfil() {
+    // Simulando uma requisição para salvar as alterações no perfil do usuário
+    this.userService.updateProfile({ name: this.userName }).subscribe(() => {
+      console.log('Perfil atualizado com sucesso!');
+    });
+  }
 }
