188b09439bd6bf3a806a94afc02bcd37c2989e0c
victorcipriano360 <125392545+victorcipriano360@users.noreply.github.com>
2023-11-20 18:50:13 -0300

    Patch 20.11.23 18.49
    Retirando a tab pagamento, e implementando no botão. Organizando a separação da tab 2. Pagina home tab1 limpa, refazer tudo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  anuncios = [
-    { imagemUrl: 'caminho_para_imagem_1.jpg' },
-    { imagemUrl: 'caminho_para_imagem_2.jpg' },
-    { imagemUrl: 'caminho_para_imagem_3.jpg' },
-    // Adicione mais URLs de imagens conforme necessário
-  ];
+
 }
