f1fb4a13cb14935fd41d322417409628ffa045bc
victorcipriano360 <victorcipriano337@gmail.com>
2023-11-24 12:47:34 -0300

    Css pt1

--- src/app/pagamento/pagamento.page.ts
+++ src/app/pagamento/pagamento.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagamento',
   templateUrl: './pagamento.page.html',
   styleUrls: ['./pagamento.page.scss'],
 })
-export class PagamentoPage implements OnInit {
+export class PagamentoPage {
 
-  constructor() { }
+  valorTotal: number = 0; // Valor total que será enviado para a página de pagamento
 
-  ngOnInit() {
+  constructor(private router: Router) {}
+
+  // Função que redireciona para a página de pagamento com o valor total
+  irParaPagamento() {
+    // Enviar o valor total para a página de pagamento (usando queryParams)
+    this.router.navigate(['/pagina-pagamento'], {
+      queryParams: {
+        valorTotal: this.valorTotal.toFixed(2), // Arredondar para 2 casas decimais
+      },
+    });
   }
-
 }
